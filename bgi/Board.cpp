383ec4aac2172dd652c5d8b1eb814c1efa88bf91
ammararfan <63009631+ammararfan@users.noreply.github.com>
2020-05-03 17:21:53 +0500

    Updated Board cpp

--- bgi/Board.cpp
+++ bgi/Board.cpp
@@ -1,1 +1,70 @@
 #include "Board.h"
+
+void Board::init()
+{
+}
+int Board::RollDice()
+{
+	return 0;
+}
+bool Board::IsValidSelection()
+{
+	return false;
+}
+bool Board::IsValidDestination()
+{
+	return false;
+}
+void Board::PrintBoard()
+{
+
+}
+void Board::Play()
+{
+
+}
+void Board::Move()
+{
+
+}
+void Board::UpdateBoard()
+{
+
+}
+void Board::IsWin()
+{
+
+}
+void Board::TurnChange()
+{
+
+}
+void Board::DisplayMessage()
+{
+
+}
+void Board::DisplayWinnerMessage()
+{
+
+}
+void Board::DisplayKillMessage()
+{
+
+}
+void Board::SelectPiece()
+{
+
+}
+void Board::SelectDestination()
+{
+
+}
+bool Board ::KillHappen()
+{
+	return false;
+}
+
+void  Board::GameOver()
+{
+
+}
